8ad622a0bd2c0ee90f3a0b37d9858565ccb21eae
Ian Chen <barkatthetree@gmail.com>
2018-05-04 02:24:28 +0800

    More css changes to Pepo

--- portfolio/pepo/pepo.js
+++ portfolio/pepo/pepo.js
@@ -5,3 +5,12 @@ var divs = document.getElementsByClassName("stack-logo");
 		trigger: "hover focus"
 	});
 });
+
+var vids = document.getElementsByClassName("stills-vid");
+[...vids].forEach(div => {
+	var x = new Tooltip(div, {
+		title: div.getAttribute("data-tooltip"),
+		trigger: "hover focus"
+	});
+	console.log(x);
+});
