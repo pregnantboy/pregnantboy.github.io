637bb9610825664d821118c9dae0b6a4d7302fa3
Ian Chen <barkatthetree@gmail.com>
2018-06-13 15:24:51 +0800

    Fixed something

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -183,8 +183,7 @@ function pushState(folder) {
 	// window.location.hash = hash;
 	let urlSuffix = "/";
 	if (folder.url) {
-		urlSuffix = folder.url.replace("/index.html", "");
-		urlSuffix = folder.url.replace(/^https?:\/\//, "/portfolio/");
+		urlSuffix = folder.url.replace("/index.html", "").replace(/^https?:\/\//, "/portfolio/");
 	}
 	history.pushState(folder, folder["folder-name"], urlSuffix);
 }
